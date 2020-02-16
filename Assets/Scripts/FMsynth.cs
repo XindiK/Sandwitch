5d4a3fe45c874d401166e32dd3b544e97360a108
XindiK <o_okang@hotmail.com>
2020-02-15 17:55:13 -0800

    Trying to tune the modulation to sound more logical
    switched audioX with micFreqency

--- Assets/Scripts/FMsynth.cs
+++ Assets/Scripts/FMsynth.cs
@@ -6,11 +6,13 @@ public class FMsynth : MonoBehaviour
 {
     public static float audioX;
     public float micFrequency;
-    
-    [Range(0, 1)]
-    public float amplitude1;
-    [Range(0, .1f)]
-    public float amplitude2;
+
+    //[Range(0, 1)]
+    //public float amplitude1;
+    //[Range(0, .1f)]
+    //public float amplitude2;
+    //public float amplitude1 = 0.5f;
+    //public float amplitude2 = 0.1f;
 
     int timeIndex = 0;
 
@@ -27,8 +29,11 @@ public class FMsynth : MonoBehaviour
         
         for (int i = 0; i < data.Length; i += channels)
         {
-            float fmfreq = audioX * CreateSine(timeIndex, micFrequency, 44100, amplitude2);
-            data[i] = CreateSine(timeIndex, fmfreq, 44100, amplitude1);
+            //float fmfreq = audioX * CreateSine(timeIndex, micFrequency, 44100, amplitude2);
+            //data[i] = CreateSine(timeIndex, fmfreq, 44100, amplitude1);
+            //float fmfreq = audioX * CreateSine(timeIndex, micFrequency, 44100, 0.5f);
+            float fmfreq = micFrequency * CreateSine(timeIndex, audioX, 44100, 0.5f);
+            data[i] = CreateSine(timeIndex, fmfreq, 44100, 0.1f);
 
             if (channels == 2)
                 data[i + 1] = data[i];
