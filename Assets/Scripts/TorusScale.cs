ef0e98535947344673b5a615050c26f27d9b6801
XindiK <o_okang@hotmail.com>
2020-02-28 19:00:09 -0800

    Added torus to disks
    Next step:
    - map no. of torus to frequency and size to amplitude

---
+++ Assets/Scripts/TorusScale.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorusScale : MonoBehaviour
+{
+    public GameObject Torus;
+    public GameObject Mic;
+    public float mult;
+    private myMixer _myMixer;
+    private float rotationPitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mic = GameObject.Find("Mic");
+        _myMixer = Mic.GetComponent<myMixer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationPitch = _myMixer.getPitchFromRotation();
+        mult = _myMixer.Remap(rotationPitch, 0.75f, 1.5f, 0.1f, 1);
+        transform.localScale = new Vector3(mult, mult, mult);
+    }
+}
