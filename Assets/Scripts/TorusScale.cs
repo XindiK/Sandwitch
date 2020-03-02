f05242ac70b2db5c1315a5d608a814fedc21a1e2
XindiK <o_okang@hotmail.com>
2020-03-01 19:38:26 -0800

    Fixed Trailrenderer problem, moved particles to outside, mapped room to y axis rotaion
    -for trail renderer just had to re-set project setting/time
    -rotation to sound mapping remains a problem.

--- Assets/Scripts/TorusScale.cs
+++ Assets/Scripts/TorusScale.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class TorusScale : MonoBehaviour
 {
-    public GameObject Torus;
+    public GameObject Prefab;
     public GameObject Mic;
     public float mult;
     private myMixer _myMixer;
     private float rotationPitch;
+    private float diskX;
+    private float diskY;
+    private float diskZ;
 
     // Start is called before the first frame update
     void Start()
     {
         Mic = GameObject.Find("Mic");
         _myMixer = Mic.GetComponent<myMixer>();
+        diskX = Prefab.transform.localScale.x;
+        diskY = Prefab.transform.localScale.y;
+        diskZ = Prefab.transform.localScale.z;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         rotationPitch = _myMixer.getPitchFromRotation();
-        mult = _myMixer.Remap(rotationPitch, 0.75f, 1.5f, 0.1f, 1);
-        transform.localScale = new Vector3(mult, mult, mult);
+        mult = _myMixer.Remap(rotationPitch, 0.75f, 1.5f, 5f, 10);
+        transform.localScale = new Vector3(diskX*mult, diskY * mult, diskZ * mult);
     }
 }
--- Assets/Scripts/myMixer.cs
+++ Assets/Scripts/myMixer.cs
@@ -8,7 +8,11 @@ using UnityEngine;
 public class myMixer : MonoBehaviour
 {
     public float rotationPitch;
-    public float normRot;
+    public float rotationRoom;
+    public float rotationDecay = 1;
+    public float normX;
+    public float normY;
+    public float normZ;
 
     private Vector3 rotation;
     private AudioMixer audioMixer;
@@ -25,29 +29,36 @@ public class myMixer : MonoBehaviour
     void Update()
     {
         rotation = GetComponent<Transform>().rotation.eulerAngles;
-        normRot = Mathf.Abs(rotation.x) % 360;
+        normX = Mathf.Abs(rotation.x) % 360;
+        normY = Mathf.Abs(rotation.y) % 360;
+        normZ = Mathf.Abs(rotation.z) % 360;
+
         //float rotationX = Remap(Mathf.Abs(rotation[0]) % 360, 0, 360, 0.5f,2.0f);
         float rotationPitch = getPitchFromRotation();
+        float rotationRoom = Remap(normY, 1, 360, -10000, 0);
 
         Debug.Log("rotationPitch: " + rotationPitch);
         //Debug.Log("rotation: " + rotation);
         //Debug.Log("rotation.x: " + rotation.x);
         //Debug.Log("abs: " + Mathf.Abs(rotation[0]));
-        //Debug.Log("normRot: " + normRot);
+        //Debug.Log("normX: " + normX);
         audioSource.outputAudioMixerGroup.audioMixer.SetFloat("myPitch", rotationPitch);
+        audioSource.outputAudioMixerGroup.audioMixer.SetFloat("myRoom", rotationRoom);
+        audioSource.outputAudioMixerGroup.audioMixer.SetFloat("myDecay", rotationDecay);
 
     }
 
     public float getPitchFromRotation()
     {
-        if (normRot >= 0 && normRot <= 180){
-            return Remap(normRot, 0, 180, 1.0f, 2.0f);
+        if (normX >= 0 && normX <= 180){
+            return Remap(normX, 0, 180, 1.0f, 2.0f);
         }
         else //bwtn 180 - 360
         {
-            return Remap(normRot, 180, 360, 0.5f, 1.0f);
+            return Remap(normX, 180, 360, 0.5f, 1.0f);
         }
     }
+
 
     public float Remap(float value, float a1, float b1, float a2, float b2)
     {
