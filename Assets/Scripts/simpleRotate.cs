223b6323ff9fa09554846411b5f5e6461ea7b38e
XindiK <o_okang@hotmail.com>
2020-03-04 14:19:28 -0800

    trail rendering for lissajous shape
    rotate disk on 2 axis: x and y
    control speed of either rotation

---
+++ Assets/Scripts/simpleRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleRotate : MonoBehaviour
+{
+    float mytime;
+    public float speed1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mytime = Time.deltaTime;
+        speed1 = 600f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, speed1 * mytime, 0));
+    }
+}
